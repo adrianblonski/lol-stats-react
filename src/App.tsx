e7bf9229d23e9567643779c2698435ffefdf8de4
adrianblonski <adblonski1@gmail.com>
2019-10-17 18:00:15 +0200

    Navbar and search form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import SiteNavbar from './components/navbar/main';
 
 class App extends Component {
+
+  onSearch = (id: number, server: string) => {
+    console.log(id + ", " + server);
+  }
+
   render() {
     return (
       <div>
-        
+        <SiteNavbar onSearch={this.onSearch} />
       </div>
     );
   }
---
+++ src/components/navbar/main.tsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import ComboBox from './combobox';
+
+import './style.css';
+
+interface Props {
+  onSearch: (server: number, name: string) => void
+}
+
+interface State {
+  server: number,
+  name: string
+}
+
+const serverNames = ["EUNE", "EUW", "NA", "LAS", "LAN", "OCE", "KR", "RU", "BR", "TR"];
+
+class SiteNavbar extends Component<Props, State> {
+  summonerRef: React.RefObject<HTMLInputElement>;
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      server: 0
+    }
+
+    this.summonerRef = React.createRef();
+  }
+
+  onServerChange = (id: number): void => {
+    this.setState({
+      server: id,
+      name: this.state.name
+    });
+  }
+
+  onSearchClick = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    const inputValue = this.summonerRef.current.value;
+    this.props.onSearch(this.state.server, inputValue);
+    this.setState({
+      name: inputValue,
+      server: this.state.server
+    });
+  }
+
+  render() {
+    return (
+      <div className="navbar">
+        <div className="logo-small">
+          <h4>LS</h4>
+        </div>
+        <div className="logo">
+          <h4>LoL Stats</h4>
+        </div>
+        <form onSubmit={this.onSearchClick} className="frm-search">
+          <ComboBox options={serverNames} onChange={this.onServerChange} />
+          <input type="text" ref={this.summonerRef} />
+          <input type="submit" value="GO" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SiteNavbar
